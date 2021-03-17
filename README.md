6ce29c5a0906bab4c79894c72e64c4c4a1ae5c2d
Matheus Lima <mfoox@protonmail.com>
2021-03-17 10:12:10 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Calcular Mé
+
+Realizar o cálculo de dois produtos e verificar qual compensa mais, baseado no seu preço e quantidade de ml
+
+Meu primeiro projeto desenvolvido do 0
+
+## Objetivo
+
+Colocar todo meu conhecimento em prática.
+
+
---
+++ lib/widgets/sucess.widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:me/widgets/loading-button.dart';
+import 'package:me/pages/home.page.dart';
+
+class Sucess extends StatelessWidget {
+  var result = "";
+  var result1 = "";
+  var precounit1 = "";
+  var precounit2 = "";
+
+  Function reset;
+  Sucess({
+    @required this.result,
+    @required this.result1,
+    @required this.reset,
+    @required this.precounit1,
+    @required this.precounit2,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 320,
+      margin: EdgeInsets.only(
+        top: 20,
+        left: 30,
+        right: 30,
+        bottom: 120,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Resultado:',
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 25,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            '$result1',
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 15,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            '$result',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          LoadingButton(
+            busy: false,
+            func: reset,
+            text: 'CALCULAR NOVAMENTE',
+            invert: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
